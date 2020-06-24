cfc2437f911f0bbda2ffd3ac511d9dd4c97cc6f8
Ertuğrul Güler <ertglr.61@gmail.com>
2020-06-24 15:02:07 +0300

    single responsibility açıklaması

--- SOLID-Principles.SingleResponsibility/Models/User.cs
+++ SOLID-Principles.SingleResponsibility/Models/User.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace SOLID_Principles.SingleResponsibility.Models
 {
+
+    //bu alanda city gerekli değildir. City daha çok adres için gereklidir ve adress adında bir class oluşturup oraya eklenmesi daha mantıklıdır. Daha sonra zipcode, street vb gibi yeni propertyler de rahatlıkla eklenebilir. 
+    //Aynı şekilde ChangeAddress, Login ve Logout işlemleri ne kadar User classı üzerinden yapılsa da tamamıyla bu class ile ilgili değildir. O yüzden bu classın sorumlulukları düzenlenmelidir.
     class User
     {
         public int Id { get; set; }
