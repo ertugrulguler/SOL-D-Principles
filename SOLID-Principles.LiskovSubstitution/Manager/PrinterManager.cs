edb5fa180cdfdf65ecc21fa2cd92b95ef85742da
Ertuğrul Güler <ertglr.61@gmail.com>
2020-06-24 20:08:06 +0300

    Print Manager tanımlandı.

---
+++ SOLID-Principles.LiskovSubstitution/Manager/PrinterManager.cs
@@ -0,0 +1,27 @@
+﻿using SOLID_Principles.LiskovSubstitution.Models.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Principles.LiskovSubstitution.Manager
+{
+    class PrinterManager
+    {
+        HPPrinter _hpPrinter = new HPPrinter();
+        CanonPrinter _canonPrinter = new CanonPrinter();
+
+        public void Print(string value)
+        {
+            //burası sıkıntısız çalışır.
+            _canonPrinter.Scan(value);
+            _canonPrinter.Print(value);
+
+
+
+            _hpPrinter.Print(value);
+            //scan özelliği olmadığı için NotImplementException hatası alınır.
+            _hpPrinter.Scan(value);
+        }
+        
+    }
+}
