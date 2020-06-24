644504c91235c53b211ed6ee5aa940034bab0d66
Ertuğrul Güler <ertglr.61@gmail.com>
2020-06-24 14:57:46 +0300

    User classı eklendi.

---
+++ SOLID-Principles.SingleResponsibility/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Principles.SingleResponsibility.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+        public void ChangeAddress(string city)
+        {
+            //bu alanda addresi değiştirecek kodlar yazılır...
+        }
+
+        public void Login(string username, string password)
+        {
+            //bu alanda bir userın login olmasıyla ilgili işlemler yapılır...
+        }
+
+        public void Logout(string username)
+        {
+            //bu alanda logout olma işlemi yapılır...
+        }
+    }
+
+
+}
