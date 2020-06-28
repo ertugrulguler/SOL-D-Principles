f782bd3ee3a43001cfae34aa093fe3a22eeea824
Ertuğrul Güler <ertglr.61@gmail.com>
2020-06-28 17:10:31 +0300

    Dependency Inversion eklendi.

---
+++ SOLID-Principle.DependencyInversion/Log Manager/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Principle.DependencyInversion.Log_Manager
+{
+    class Logger
+    {
+
+        //şuanda Dependency Inversion prensibine uymuyoruz. Çünkü Logger classı şuanda XmlLog classına bağımlıdır. 
+        private readonly XmlLog xmlLog = new XmlLog();
+
+
+        public void Log(string value)
+        {
+            xmlLog.Log(value);
+        }
+
+    }
+}
---
+++ SOLID-Principle.DependencyInversion/Log Manager/XmlLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Principle.DependencyInversion.Log_Manager
+{
+    public class XmlLog
+    {
+        public bool Log(string value)
+        {
+            //ilgili işlemler
+            return true;
+        }
+    }
+}
