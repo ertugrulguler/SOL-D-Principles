e247d5fda50a5fadf807efb46820d73a64422dbc
Ertuğrul Güler <ertglr.61@gmail.com>
2020-06-28 16:30:44 +0300

    Liskov substitution tamamlandı.

--- SOLID-Principles.LiskovSubstitution/Manager/PrinterManager.cs
+++ SOLID-Principles.LiskovSubstitution/Manager/PrinterManager.cs
@@ -14,7 +14,9 @@ namespace SOLID_Principles.LiskovSubstitution.Manager
         
         public void Print(string value)
         {
-            //burası sıkıntısız çalışır.
+            //Liskov substitution  prensibine göre kalıtım sırasındaki bir alt sınıf üst sınıfın tüm özelliklerini tüm metotlarını
+            //kullanabilmelidir. Printerlardan biri scan özelliğine sahip değildi. Base printerda önceki yapıda yanlış tanımlanmıştı.
+            //artık scan özelliği IScan interfacesine alınarak, scanlama özelliği olan printer classları bu interfaceden de implement edilmelidir. Böylece daha doğru bir yapıya kavuşmuş oluruz.
             _canonScanner.Scan(value);
             _canonPrinter.Print(value);
 
