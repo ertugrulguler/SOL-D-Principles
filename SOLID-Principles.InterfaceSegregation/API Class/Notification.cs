7626f9252b3d8c9fb987c6fe26cbf9a2e2c9cc9f
Ertuğrul Güler <ertglr.61@gmail.com>
2020-06-28 16:41:24 +0300

    Interface Segregation eklendi.

---
+++ SOLID-Principles.InterfaceSegregation/API Class/Notification.cs
@@ -0,0 +1,31 @@
+﻿using SOLID_Principles.InterfaceSegregation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Principles.InterfaceSegregation.API_Class
+{
+    class Notification : IBaseApi
+    {
+        //Bildirimler için sadece get işlemi yapılır. Peki burada neden diğerleri de yer alıyor? Çünkü Interface Segregation prensibine uymuyoruz.
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Post(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SOLID-Principles.InterfaceSegregation/Interfaces/IBaseApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Principles.InterfaceSegregation.Interfaces
+{
+    interface IBaseApi
+    {
+        void Put(int id);
+        void Delete(int id);
+        void Post(int id);
+        void Get(int id);
+    }
+}
